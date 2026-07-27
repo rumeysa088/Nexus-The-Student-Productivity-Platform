ba49033c3278516e585c718086db20b82d36727e
rumeysa088 <k250585@nu.edu.pk>
2026-07-27 15:28:24 +0500

    Initialize project structure

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "==================================" << endl;
+    cout << "      Welcome to NEXUS" << endl;
+    cout << " Your Academic Command Center" << endl;
+    cout << "==================================" << endl;
+
+    return 0;
+}
